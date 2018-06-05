02db9f891e6f6575f8bcfe4d29ab562ed208d34f
Wayne Franz <umfranzw@cs.umanitoba.ca>
2018-06-05 13:28:24 -0500

    Added output_rate bar graph to visualization images.

--- vis/vis.py
+++ vis/vis.py
@@ -9,9 +9,32 @@ DB_FILE = '../data/dbs/run0.db'
 IMG_DIR = '../data/images'
 GENE_WIDTH = 0.2
 GENE_HEIGHT = 0.25
-PLOT_WIDTH = 8
-PLOT_HEIGHT = 8
-KELLY_COLOURS = ['#F2F3F4', '#222222', '#F3C300', '#875692', '#F38400', '#A1CAF1', '#BE0032', '#C2B280', '#848482', '#008856', '#E68FAC', '#0067A5', '#F99379', '#604E97', '#F6A600', '#B3446C', '#DCD300', '#882D17', '#8DB600', '#654522', '#E25822', '#2B3D26'] #Kelly's 22 colours of max contrast
+FIG_WIDTH = 8
+FIG_HEIGHT = 10
+BAR_WIDTH = 0.8
+#Kelly's 22 colours of max contrast
+KELLY_COLOURS = ['#F2F3F4',
+                 #'#222222',
+                 '#F3C300',
+                 '#875692',
+                 '#F38400',
+                 '#A1CAF1',
+                 '#BE0032',
+                 '#C2B280',
+                 '#848482',
+                 '#008856',
+                 '#E68FAC',
+                 '#0067A5',
+                 '#F99379',
+                 '#604E97',
+                 '#F6A600',
+                 '#B3446C',
+                 '#DCD300',
+                 '#882D17',
+                 '#8DB600',
+                 '#654522',
+                 '#E25822',
+                 '#2B3D26']
 LINE_STYLES = ['-', '--', '-.', ':']
 
 def plot_avg_fitness(conn):
@@ -48,6 +71,8 @@ def _get_conc_sql(run):
     return sql
 
 def _plot_concs(ga_step, reg_step, pop_index, run, conn, ax):
+    plt.sca(ax) #note: sca=set current axis
+    
     sql = 'SELECT p.pid, {} FROM grn JOIN protein_state ps ON grn.id = ps.grn_id JOIN protein p ON ps.protein_id = p.id WHERE grn.ga_step = ? AND ps.reg_step = ? AND grn.pop_index = ?;'.format(_get_conc_sql(run))
     rs = conn.execute(sql, (ga_step, reg_step, pop_index))
     colours = {} #maps pid to and index of KELLY_COLOURS
@@ -62,9 +87,14 @@ def _plot_concs(ga_step, reg_step, pop_index, run, conn, ax):
         if style_index == 0:
             colours[pid] = colour_index
 
+    plt.xlabel('gene')
+    plt.ylabel('concentration')
+
     return colours
 
 def _draw_bindings(ga_step, reg_step, pop_index, run, conn, ax, colours):
+    plt.sca(ax)
+    
     sql = ('SELECT p.pid FROM grn JOIN gene g ON grn.id = g.grn_id JOIN ' +
            'gene_state gs ON gs.gene_id = g.id JOIN ' +
            'protein p ON gs.bound_protein = p.id ' + #note: gs.bound_protein is a *db* id (not a simulation id)
@@ -82,36 +112,73 @@ def _draw_bindings(ga_step, reg_step, pop_index, run, conn, ax, colours):
                 colour_index = colours[pid]
                 colour = KELLY_COLOURS[colour_index]
             else:
-                #this isn't perfect, but it will do
-                colour_index = (len(colours) + 1) % len(KELLY_COLOURS)
-                colour = KELLY_COLOURS[colour_index]
+                colour = 'pink'
             
         rect = mpatches.Rectangle((i * GENE_WIDTH, -GENE_HEIGHT), GENE_WIDTH, GENE_HEIGHT, facecolor=colour, edgecolor='black')
         ax.add_patch(rect)
         if text:
-            plt.text(i * GENE_WIDTH + GENE_WIDTH / 2.2, -GENE_HEIGHT + GENE_HEIGHT / 2.9, text)
+            ax.text(i * GENE_WIDTH + GENE_WIDTH / 2.2, -GENE_HEIGHT + GENE_HEIGHT / 2.9, text)
 
 def _draw_outputs(ga_step, reg_step, pop_index, run, conn, ax, colours):
-    xs = [1, 1, 1, 1, 1, 1, 1, 1]
-    ax.bar(xs, run.max_protein_conc)
+    plt.sca(ax)
+    
+    sql = ('SELECT p.pid, g.output_rate FROM grn JOIN gene g ON grn.id = g.grn_id JOIN ' +
+           'gene_state gs ON gs.gene_id = g.id JOIN ' +
+           'protein p ON gs.active_output = p.id ' + #note: gs.active_output is a *db* id (not a simulation id)
+           'WHERE grn.ga_step = ? AND grn.pop_index = ? AND g.pos = ? AND gs.reg_step = ?;')
+
+    ys = []
+    bar_colours = []
+    bar_texts = []
+    for i in range(run.num_genes):
+        rs = conn.execute(sql, (ga_step, pop_index, i, reg_step))
+        row = rs.fetchone()
+        colour = 'white'
+        text = ''
+        if row:
+            pid, output_rate = row
+            ys.append(output_rate)
+            text = "{}".format(pid)
+            if pid in colours:
+                colour_index = colours[pid]
+                colour = KELLY_COLOURS[colour_index]
+            else:
+                colour = 'pink'
+                
+            bar_colours.append(colour)
+            bar_texts.append(text)
+        else:
+            ys.append(0)
+            bar_texts.append('')
+            bar_colours.append('white')
+    
+    xs = [i for i in range(run.num_genes)]
+    ax.bar(xs, ys, BAR_WIDTH, color=bar_colours)
+    plt.xlabel('gene')
+    plt.ylabel('output rate')
+
+    for i in range(run.num_genes):
+        if bar_texts[i]:
+            ax.text(xs[i], ys[i] / 2, bar_texts[i])
 
 def draw_grn(ga_step, reg_step, pop_index, run, conn):
-    fig, ax = plt.subplots(figsize=(PLOT_WIDTH, PLOT_HEIGHT))
+    fig, ax = plt.subplots(2, 1, figsize=(FIG_WIDTH, FIG_HEIGHT))
     for i in range(run.num_genes):
         rect = mpatches.Rectangle((i * GENE_WIDTH, -GENE_HEIGHT), GENE_WIDTH, GENE_HEIGHT, fill=None)
-        ax.add_patch(rect)
+        ax[0].add_patch(rect)
 
-    ax.get_xaxis().set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.set_ylim([-GENE_HEIGHT - 0.01, run.max_protein_conc])
-    ax.set_xlim([0, run.num_genes * GENE_WIDTH + 0.01])
+    ax[0].get_xaxis().set_visible(False)
+    ax[0].spines['right'].set_visible(False)
+    ax[0].spines['top'].set_visible(False)
+    ax[0].spines['bottom'].set_visible(False)
+    ax[0].set_ylim([-GENE_HEIGHT - 0.01, run.max_protein_conc])
+    ax[0].set_xlim([0, run.num_genes * GENE_WIDTH + 0.01])
 
-    colours = _plot_concs(ga_step, reg_step, pop_index, run, conn, ax)
-    _draw_bindings(ga_step, reg_step, pop_index, run, conn, ax, colours)
+    colours = _plot_concs(ga_step, reg_step, pop_index, run, conn, ax[0])
+    _draw_bindings(ga_step, reg_step, pop_index, run, conn, ax[0], colours)
+    _draw_outputs(ga_step, reg_step, pop_index, run, conn, ax[1], colours)
 
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
     #plt.tight_layout()
 
